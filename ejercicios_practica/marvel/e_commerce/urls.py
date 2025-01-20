fc59206aa9da1779cdbbb7b0805d74f1d53d8755
arianaminakian <142285097+arianaminakian@users.noreply.github.com>
2025-01-20 19:56:07 -0300

    Add files via upload

---
+++ ejercicios_practica/marvel/e_commerce/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from e_commerce.api.views import *
+
+
+urlpatterns = [
+    path('comic-list/', comic_list_api_view, name="comic_list_api_view"),
+    path('comic-retrieve/', comic_retrieve_api_view, name="comic_retrieve_api_view"),
+    path('comic-create/', comic_create_api_view, name="comic_create_api_view"),
+    path('comic-filtered/',comic_list_filtered_api_view, name= "comic_list_filtered_api_view"),
+    path('comic-stock-filtered/',comic_stock_filtered_api_view, name="comic_stock_filtered_api_view")
+]
